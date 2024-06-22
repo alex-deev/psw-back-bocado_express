a4ad82aa4ea8351512dccb9b9994917ffdd0dac9
ax-star <ax.onestar@gmail.com>
2024-06-21 21:45:16 -0300

    Se modifica db.ts
    Se realiza la conexión la base de datos remota.

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,13 @@
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource ({
+    // MySql DB from Avien
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'psw',
-    password: 'psw',
-    database: 'psw',
+    host: 'mysql-bocado-express-bd-bocado-express.d.aivencloud.com',
+    port: 18169,
+    username: 'bocado',
+    password: 'AVNS_qUddzBaFj9-q09cnB4R',
+    database: 'bocado_express',
     synchronize: true,
     // entities: []
 });
